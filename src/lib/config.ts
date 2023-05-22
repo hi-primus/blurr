47a44e18bffcfe59e352decc3995b78f6bf6cd30
luis11011 <luis11011@users.noreply.github.com>
2023-05-22 21:00:19 +0000

    Update optimus version

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,1 @@
-export const OPTIMUS_WHEEL_URL = 'https://test-files.pythonhosted.org/packages/74/41/ff48d76789aed550c785b833303e973ec1f5480aaf3d6b75743db56f7747/pyoptimus-0.1.4061-py3-none-any.whl';
+export const OPTIMUS_WHEEL_URL = 'https://test-files.pythonhosted.org/packages/ab/67/f8e8cf7777857f2cb15e42d84b0f535e1ab53ff773a4c6c060a11274d11a/pyoptimus-0.1.4062-py3-none-any.whl';
