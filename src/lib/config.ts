d427d353cbb3a4c8e83c46cc7a3d32c4a7541976
Luis Aguirre <luisaguirre027@gmail.com>
2023-05-22 16:50:17 -0400

    Use single line in config.ts file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,1 @@
-export const OPTIMUS_WHEEL_URL =
-  'https://test-files.pythonhosted.org/packages/74/41/ff48d76789aed550c785b833303e973ec1f5480aaf3d6b75743db56f7747/pyoptimus-0.1.4061-py3-none-any.whl';
+export const OPTIMUS_WHEEL_URL = 'https://test-files.pythonhosted.org/packages/74/41/ff48d76789aed550c785b833303e973ec1f5480aaf3d6b75743db56f7747/pyoptimus-0.1.4061-py3-none-any.whl';
